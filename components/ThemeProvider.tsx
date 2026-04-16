db2a4b12c12d18f25152bc8985eb7ee3c3af036b
Prakhar Dubey <prakhardubey2002@gmail.com>
2026-04-16 21:06:11 +0530

    feat(theme) : theme support added

---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import type { PropsWithChildren } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+export function ThemeProvider({ children }: PropsWithChildren) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
+
